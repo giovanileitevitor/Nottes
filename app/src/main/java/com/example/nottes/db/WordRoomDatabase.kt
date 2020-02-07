dccc2466ef230dff0a94a21c958fa36ed9c2176b
giovanileitevitor@gmail.com <giovanileitevitor@gmail.com>
2020-02-07 12:25:10 -0300

    Initial commit

---
+++ app/src/main/java/com/example/nottes/db/WordRoomDatabase.kt
@@ -0,0 +1,39 @@
+package com.example.nottes.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.nottes.dao.WordDao
+import com.example.nottes.entities.Word
+
+// Annotates class to be a Room Database with a table (entity) of the Word class
+@Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
+public abstract class WordRoomDatabase : RoomDatabase() {
+
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context): WordRoomDatabase {
+            val tempInstance =
+                INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
